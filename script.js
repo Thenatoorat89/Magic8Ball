4fccb9381327f17679bbf6bfbb69b7591fd1a25e
Sebastian <116764068+Thenatoorat89@users.noreply.github.com>
2023-04-21 14:22:09 +0200

    Add files via upload
    Added Enter key event

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-const ball = document.querySelector('img')
-const input = document.querySelector('input')
-const answer = document.querySelector('.answer')
-const error = document.querySelector('.error')
+const ball = document.querySelector('img');
+const input = document.querySelector('input');
+const error = document.querySelector('.error');
+const answer = document.querySelector('.answer');
 
-const answersArr = ['Tak!', 'Nie.', 'Może.', 'Ciężko powiedzieć...', 'Chyba nie chcesz znać odpowiedzi :/']
-
-const checkInput = () => {
-    if(input.value !== '' && input.value.slice(-1) === '?'){
-        getAnswer();
-        error.textContent = ''
-    }else if(input.value !== '' && input.value.slice(-1) !== '?'){
-        error.textContent = 'Pytanie musi kończyć się znakiem "?".'
-        answer.textContent = ''
-    }      
-    else {
-        error.textContent = 'Musisz zadać pytanie!'
-        answer.textContent= ''
-    }
-    ball.classList.remove('shake-animation')
-}
-
-const getAnswer = () => {
-    const number = Math.floor(Math.random()*5)
-    answer.innerHTML = `<span>Odpowiedź:</span> ${answersArr[number]}`
-}
+const answersArr = [
+	'Tak!',
+	'Nie.',
+	'Może.',
+	'Ciężko powiedzieć...',
+	'Chyba nie chcesz wiedzieć... :/',
+	'Niewykluczone.',
+];
 
 const shakeBall = () => {
-    ball.classList.add('shake-animation')
-    setTimeout(checkInput,1000)
-}
+	ball.classList.add('shake-animation');
+	setTimeout(checkInput, 1000);
+};
 
-ball.addEventListener('click', shakeBall)
+const getAnswer = () => {
+	const number = Math.floor(Math.random() * 6);
+	answer.innerHTML = `<span>Odpowiedź to:</span> ${answersArr[number]}`;
+};
+
+const checkInput = () => {
+	if (input.value !== '' && input.value.slice(-1) === '?') {
+		getAnswer();
+		error.textContent = '';
+	} else if (input.value !== '' && input.value.slice(-1) !== '?') {
+		error.textContent = 'Pytanie musi kończyć się znakiem "?"';
+		answer.textContent = '';
+	} else {
+		error.textContent = 'Nie zadałeś pytania...';
+		answer.textContent = '';
+	}
+	ball.classList.remove('shake-animation');
+};
+
+const enterKey = e => {
+	if (e.key === 'Enter') {
+		shakeBall();
+	}
+};
+
+ball.addEventListener('click', shakeBall);
+input.addEventListener('keyup', enterKey);
